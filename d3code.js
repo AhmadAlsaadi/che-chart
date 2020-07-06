fcde9316be8083fed709703124447152215bda05
Aseel Alsaadi <aseel.alsaadi0@gmail.com>
2020-07-06 04:41:01 +0300

    courses completed with proper format

--- d3code.js
+++ d3code.js
@@ -1,4 +1,11 @@
 var preReq=[];
+curr.forEach(function(d){
+	d.courses.forEach(function(p){
+		if(p.pre!=undefined){
+		preReq.push(p.pre)}
+	})
+})
+console.log(preReq)
 const container=d3.select(".grid-container")     //specify grid-container div to work with.
 
 const semesters=container.selectAll(".semester").data(curr).enter().append("div").attr("class","semester");   // creating number of semesters offered.
@@ -8,16 +15,8 @@ const courses=semesters.selectAll(".course-grid").data(d=>d.courses).enter().app
 const course=courses.selectAll(".course").data(d=>d.name).enter().append("div").attr("class",(d,i)=>course_tags[i])
 course.filter(function(d,i) { return i < 5 }).append('p').text(d=>d);
 
-console.log(d3.selectAll(".course-pre"))
- console.log(semesters.data(d=>d.pre))
-
-
-
-//console.log(d3.selectAll(".course-pre").selectAll(".required").data(function(d,i){
-    
-  //console.log(d)}).enter().append("div").attr("class","required").append("p").text(d=>d));
-
-
+const preRequisit=d3.selectAll(".course-pre").data(preReq)
+preRequisit.selectAll(".pre").data(d=>d).enter().append("div").attr("class",d=>d[1].slice(0,4)+" required").append("p").text(d=>d[0])
 
 
 
--- d3courses.js
+++ d3courses.js
@@ -2,27 +2,27 @@
 var allcourses={
     ENCH:{
         ENCH110:["1","ENCH-110","3","Material Science","S",[1,2]],
-        ENCH201:["ench","2","ENCH-201","4","Intro. to Chem. Eng.","F",[1]],
-        ENCH202:["ench","3","ENCH-202","3","Chem. Eng. Thermodynamics I","S",[1]],
-        ENCH204:["ench","4","ENCH-204","3","Momentum Transfer","S",[1]],
-        ENCH302:["ench","5","ENCH-302","3","Chem. Eng. Thermodynamics II","F",[1]],
-        ENCH305:["ench","6","ENCH-305","3","Heat Transfer","F",[1]],
-        ENCH306:["ench","7","ENCH-306","3","Mass Transfer","F",[1]],
-        ENCH320:["ench","8","ENCH-320","3","Numerical Methods","S",[1]],
-        ENCH421:["ench","9","ENCH-421","3","Process Control","F",[1]],
-        ENCH325:["ench","10","ENCH-325","1","Chem. Eng. Computer Lab","S",[1]],
-        ENCH330:["ench","11","ENCH-330","3","Separation Processes","S",[1]],
-        ENCH341:["ench","12","ENCH-341","1","Chem. Eng. Lab I","F",[1]],
-        ENCH342:["ench","13","ENCH-342","1","Chem. Eng. Lab II","S",[1]],
-        ENCH350:["ench","14","ENCH-350","3","Chem. Reaction Eng.","S",[1]],
-        ENCH390:["ench","15","ENCH-390","0","Summer Training","Sm",[1]],
-        ENCH420:["ench","16","ENCH-420","3","Process Design & Economics","F",[1]],
-        ENCH422:["ench","17","ENCH-422","3","Process Safety Management","S",[1]],
-        ENCH426:["ench","18","ENCH-426","3","Process Integ. & Optimz.","S",[1]],
-        ENCH499:["ench","19","ENCH-499","4","Senior Design Project","F",[1]],
-        ENCHX:["ench","20","ENCH-X","3","Chem. Eng. Elective I","F",[1]],
-        ENCHXX:["ench","21","ENCH-XX","3","Chem. Eng. Elective II","S",[1]],
-        ENCHXXX:["ench","22","ENCH-XXX","3","Chem. Eng. Elective III","S",[1]],
+        ENCH201:["2","ENCH-201","4","Intro. to Chem. Eng.","F",[1]],
+        ENCH202:["3","ENCH-202","3","Chem. Eng. Thermo I","S",[1]],
+        ENCH204:["4","ENCH-204","3","Momentum Transfer","S",[1]],
+        ENCH302:["5","ENCH-302","3","Chem. Eng. Thermo II","F",[1]],
+        ENCH305:["6","ENCH-305","3","Heat Transfer","F",[1]],
+        ENCH306:["7","ENCH-306","3","Mass Transfer","F",[1]],
+        ENCH320:["8","ENCH-320","3","Numerical Methods","S",[1]],
+        ENCH421:["9","ENCH-421","3","Process Control","F",[1]],
+        ENCH325:["10","ENCH-325","1","Chem. Eng. Comp Lab","S",[1]],
+        ENCH330:["11","ENCH-330","3","Separation Processes","S",[1]],
+        ENCH341:["12","ENCH-341","1","Chem. Eng. Lab I","F",[1]],
+        ENCH342:["13","ENCH-342","1","Chem. Eng. Lab II","S",[1]],
+        ENCH350:["14","ENCH-350","3","Chem. Reaction Eng.","S",[1]],
+        ENCH390:["15","ENCH-390","0","Summer Training","Sm",[1]],
+        ENCH420:["16","ENCH-420","3","Proc Design & Econ","F",[1]],
+        ENCH422:["17","ENCH-422","3","Proc. Safety Manag","S",[1]],
+        ENCH426:["18","ENCH-426","3","Proc Integ. & Optimz.","S",[1]],
+        ENCH499:["19","ENCH-499","4","Senior Design Project","F",[1]],
+        ENCHX:["20","ENCH-X","3","Chem. Eng. Elective I","F",[1]],
+        ENCHXX:["21","ENCH-XX","3","Chem. Eng. Elective II","S",[1]],
+        ENCHXXX:["22","ENCH-XXX","3","Chem. Eng. Elect. III","S",[1]],
         
         },
     ENME:{
@@ -31,72 +31,72 @@ var allcourses={
         ENME203:["2","ENME-203","2","علم الحركة","S",[1]],
         },
     ENIE:{
-        ENIE100:["1","ENIE-100","2","Tech. Comm. Skills","F",[1,2,3]],
+        ENIE100:["1","ENIE-100","2","Tech. Comm. Skills","F",[1]],
         ENIE202:["2","ENIE-202","2","Intro. to Eng. Design","S",[1]],
-        ENIE203:["enie","3","ENIE-203","1","Prof. Ethics for Eng.","S",[1]],
-        ENIE231:["enie","4","ENIE-231","2","Probability & Statistics","S",[1]],
+        ENIE203:["3","ENIE-203","1","Prof. Ethics for Eng.","S",[1]],
+        ENIE231:["4","ENIE-231","2","Probability & Statistics","S",[1]],
         ENIE255:["5","ENIE-255","2","Eng. Economy","S",[1]]
                
         },
     ISLM:{
-        ISLM201:["islm","1","ISLM-201","3","عبادات ومعاملات","S",[1]],
-        ISLM202:["islm","2","ISLM-202","3","عبادات ومعاملات","F",[1]],
-        ISLM303:["islm","3","ISLM-303","3","عبادات ومعاملات","F",[1]],
+        ISLM201:["1","ISLM-201","3","عبادات ومعاملات","S",[1]],
+        ISLM202:["2","ISLM-202","3","عبادات ومعاملات","F",[1]],
+        ISLM303:["3","ISLM-303","3","عبادات ومعاملات","F",[1]],
                    
         },
     XXXX:{
-        XXXXI:["free","1","XXXX-001","3","Free Course I","F|S",[1]],
-        XXXXII:["free","2","XXXX-002","3","Free Course II","F|S",[1]],
+        XXXXI:["1","FREE-001","3","Free Course I","F|S",[1]],
+        XXXXII:["2","FREE-002","3","Free Course II","F|S",[1]],
                    
         },
     ENEE:{
-        ENEE101:["1","ENEE-101","3","Computer Programming","S",[1]],
-        ENEE305:["enee","2","ENEE-305","3","Fund. of Elec. Circuits","S",[1]],            
+        ENEE101:["1","ENEE-101","3","Comp. Programming","S",[1]],
+        ENEE305:["2","ENEE-305","3","Fund. of Elec. Circuits","S",[1]],            
         },
     SCCH:{
-        SCCH101:["scch","1","SCCH-101","3","General Chemistry I","F",[1]],
+        SCCH101:["1","SCCH-101","3","General Chemistry I","F",[1]],
         SCCH211:["2","SCCH-211","1","General Chemistry Lab","F",[1]],
-        SCCH213:["scch","3","SCCH-213","4","General Chemistry II","F",[1]],
-        SCCH215:["scch","4","SCCH-215","4","Organic Chemistry","F",[1]],            
+        SCCH213:["3","SCCH-213","4","General Chemistry II","F",[1]],
+        SCCH215:["4","SCCH-215","4","Organic Chemistry","F",[1]],            
         },
     SCPH:{
-        SCPH101:["scph","1","SCPH-101","4","General Physics I","F",[1]],
+        SCPH101:["1","SCPH-101","4","General Physics I","F",[1]],
         SCPH211:["2","SCPH-211","4","General Physics II","F",[1]],
         SCPH214:["3","SCPH-214","1","General Physics Lab","F",[1]],          
         },
     EDAR:{
         EDAR101:["1","EDAR-101","3","Linguistic Skills I","F",[1]],
-        EDAR202:["edar","2","EDAR-202","3","Linguistic Skills II","F",[1]],          
+        EDAR202:["2","EDAR-202","3","Linguistic Skills II","F",[1]],          
         },
     ENCI:{
         ENCI101:["1","ENCI-101","2","Eng. Mechanics Statics","F",[1]],          
         },
     EIPR:{
-        EIPR102:["eipr","1","EIPR-102","2","English Language II","F",[1]],
+        EIPR102:["1","EIPR-102","2","English Language II","F",[1]],
     },          
     CCIT:{
-        CCIT100:["ccit","1","CCIT-100","2","Computer Skills","F",[1]],          
+        CCIT100:["1","CCIT-100","2","Computer Skills","F",[1]],          
         },
     SCMT:{
         SCMT101:["1","SCMT-101","3","Calculus I","F",[1]],
         SCMT211:["2","SCMT-211","3","Calculus II","F",[1]],
         SCMT212:["3","SCMT-212","3","Calculus III","S",[1]],
         SCMT221:["4","SCMT-221","3","Linear Algebra","F",[1]],            
-        SCMT222:["scmt","5","SCMT-222","3","Differential Equations","F",[1]],            
-        SCMT223:["scmt","6","SCMT-223","3","Series & Vect. Calculus","F",[1]],            
+        SCMT222:["5","SCMT-222","3","Differential Equations","F",[1]],            
+        SCMT223:["6","SCMT-223","3","Series & Vect. Calc.","F",[1]],            
         },
     ENWS:{
-        ENWS201:["ENWS","1","ENWS-201","3","Introduction to water treatment","F",[1]],
-        ENWS301:["ENWS","2","ENWS-301","3","Thermal Desalination Technologies","F",[1]],
-        ENWS302:["ENWS","3","ENWS-302","3","Calculus III","S",[1]],
-        ENWS402:["ENWS","4","ENWS-401","3","Reverse Osmosis","F",[1]],            
-        ENWS403:["ENWS","5","ENWS-402","3","Emerging desalination Technlogies","F",[1]],            
-        ENWS411:["ENWS","6","ENWS-411","3","Water Management and Field Investigations","F",[1]],            
-        ENWS390:["ENWS","7","ENWS-390","3","Summer Training","F",[1]],            
-        ENWS499:["ENWS","8","ENWS-499","3","Project Design","F",[1]],            
-        ENWSX:["ENWS","9","ENWS-X","3","Elective course I","F",[1] ],  
-        ENWSXX:["ENWS","10","ENWS-XX","3","Elective course II","F",[1]],            
-        ENWSXXX:["ENWS","11","ENWS-XXX","3","Elective course III","F",[1]],            
+        ENWS201:["1","ENWS-201","3","Introduction to water treatment","F",[1]],
+        ENWS301:["2","ENWS-301","3","Thermal Desalination Technologies","F",[1]],
+        ENWS302:["3","ENWS-302","3","Calculus III","S",[1]],
+        ENWS402:["4","ENWS-401","3","Reverse Osmosis","F",[1]],            
+        ENWS403:["5","ENWS-402","3","Emerging desalination Technlogies","F",[1]],            
+        ENWS411:["6","ENWS-411","3","Water Management and Field Investigations","F",[1]],            
+        ENWS390:["7","ENWS-390","3","Summer Training","F",[1]],            
+        ENWS499:["8","ENWS-499","3","Project Design","F",[1]],            
+        ENWSX:["9","ENWS-X","3","Elective course I","F",[1] ],  
+        ENWSXX:["10","ENWS-XX","3","Elective course II","F",[1]],            
+        ENWSXXX:["11","ENWS-XXX","3","Elective course III","F",[1]],            
         },
     
 }; 
@@ -105,128 +105,86 @@ const course_tags=["course-no","course-code","course-credit","course-title","cou
 const curr=[
  
     {name:"semester1", courses:[
-        {name:["","Semester I"],pre:[2]},
+        {name:["","Semester I"]},
         {name:allcourses.ENIE.ENIE100,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.EDAR.EDAR101,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCMT.SCMT211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH214,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCCH.SCCH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.ENIE.ENIE202,pre:[allcourses.ENCH.ENCH110]},
+        {name:allcourses.EDAR.EDAR101,pre:[]},
+        {name:allcourses.SCMT.SCMT211,pre:[allcourses.SCMT.SCMT101]},
+        {name:allcourses.SCPH.SCPH211,pre:[allcourses.SCPH.SCPH101,allcourses.SCMT.SCMT101]},
+        {name:allcourses.SCPH.SCPH214,pre:[allcourses.SCPH.SCPH101]},
+        {name:allcourses.SCCH.SCCH211,pre:[allcourses.SCCH.SCCH101]},
+       
     ]},
     {name:"semester2", courses:[
-        {name:["","Semester II"],pre:[]},
-        {name:allcourses.ENIE.ENIE100,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.EDAR.EDAR101,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCMT.SCMT211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH214,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCCH.SCCH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.ENIE.ENIE202,pre:[allcourses.ENCH.ENCH110]},
+        {name:["","Semester II"]},
+        {name:allcourses.ENIE.ENIE255,pre:[allcourses.SCMT.SCMT101]},
+        {name:allcourses.SCMT.SCMT212,pre:[allcourses.SCMT.SCMT101]},
+        {name:allcourses.ENCI.ENCI101,pre:[allcourses.SCPH.SCPH214]},
+        {name:allcourses.ENME.ENME102
+            ,pre:[allcourses.ENCH.ENCH110]},
+        {name:allcourses.ENCH.ENCH110,pre:[allcourses.SCCH.SCCH211]},
+        {name:allcourses.ENEE.ENEE101,pre:[allcourses.SCMT.SCMT101]},
+        {name:allcourses.ENIE.ENIE202,pre:[allcourses.ENIE.ENIE100]},
     ]},
     {name:"semester3", courses:[
-        {name:["","Semester III"],pre:[]},
-        {name:allcourses.ENIE.ENIE100,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.EDAR.EDAR101,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCMT.SCMT211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCPH.SCPH214,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.SCCH.SCCH211,pre:[allcourses.ENCH.ENCH110]},
-        {name:allcourses.ENIE.ENIE202,pre:[allcourses.ENCH.ENCH110]},
-    ]}]
-/*
-    [
-        ["","Semester II"],allcourses.ENIE.ENIE100,allcourses.SCMT.SCMT212,allcourses.ENCI.ENCI101,allcourses.ENME.ENME102,allcourses.ENCH.ENCH110,allcourses.ENEE.ENEE101
-    ],
-
-    [
-        ["","Semester III"],allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110
-    ],
-
-    [
-        ["","Semester IV"],allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110,allcourses.ENCH.ENCH110
-    ],
-
-    [
-        ["","Semester V"],allcourses.ENIE.ENIE100,allcourses.EDAR.EDAR101,allcourses.SCMT.SCMT211,allcourses.SCPH.SCPH211,allcourses.SCPH.SCPH214,allcourses.SCCH.SCCH211,allcourses.ENIE.ENIE202
-    ],
-
-    ]
-
-
-
-
-
-/*
-
-curriculum=[
-    {name:"semester8",values:
-[[courses.ENCH.ENCH422,[courses.ENCH.ENCH330,courses.ENCH.ENCH420]],
-               [courses.ENCH.ENCH426,[courses.ENCH.ENCH420]],
-               [courses.ENCH.ENCHXX,[]],
-               [courses.ENCH.ENCHXXX,[]],
-               [courses.XXXX.XXXXII,[]]
-        ]},
-
-        //semester7
-        {name:"semester7", values:[[courses.ENCH.ENCH421,[courses.ENCH.ENCH350,courses.ENCH.ENCH330]],
-                [courses.ENCH.ENCH420,[courses.ENCH.ENCH330,courses.ENCH.ENCH325]],
-                [courses.ENCH.ENCH499,[]],
-                [courses.ENCH.ENCHX,[]],
-                [courses.ISLM.ISLM303,[courses.ISLM.ISLM202]],
-        [courses.ENCH.ENCH390,[]]
-            ]},
+        {name:["","Semester III"]},
+        {name:allcourses.ENCH.ENCH201,pre:[allcourses.SCCH.SCCH101]},
+        {name:allcourses.SCCH.SCCH213,pre:[allcourses.SCCH.SCCH101]},
+        {name:allcourses.SCMT.SCMT221,pre:[allcourses.SCMT.SCMT211]},
+        {name:allcourses.SCMT.SCMT222,pre:[allcourses.SCMT.SCMT211]},
+        {name:allcourses.EDAR.EDAR202,pre:[allcourses.EDAR.EDAR101]},
         
-        //semester6
-        {name:"semester6",values:[[courses.ENCH.ENCH350,[courses.ENCH.ENCH201,courses.SCMT.SCMT222]],
-                [courses.ENCH.ENCH330,[courses.ENCH.ENCH302,courses.ENCH.ENCH306]],
-                [courses.ENCH.ENCH342,[courses.ENCH.ENCH341]],
-                [courses.ENCH.ENCH325,[courses.ENCH.ENCH201]],
-                [courses.ENCH.ENCH320,[courses.ENEE.ENEE101,courses.SCMT.SCMT222]],
-                [courses.ENEE.ENEE305,[courses.SCPH.SCPH211]],
-                [courses.XXXX.XXXXI,[]]
-            ]},
-        //semester5
-        {name:"semester5", values:[[courses.ENCH.ENCH302,[courses.ENCH.ENCH202]],
-                [courses.ENCH.ENCH305,[courses.ENCH.ENCH204]],
-                [courses.ENCH.ENCH306,[courses.ENCH.ENCH204,courses.ENCH.ENCH305]],
-                [courses.ENCH.ENCH341,[courses.ENCH.ENCH201,courses.ENCH.ENCH306]],
-                [courses.ISLM.ISLM202,[courses.ISLM.ISLM201]],
-                [courses.SCCH.SCCH215,[courses.SCCH.SCCH213]],
-            ]},
-        //semester4
-        {name:"semester4", values:[[courses.ENCH.ENCH202,[courses.ENCH.ENCH201]],
-                [courses.ENCH.ENCH204,[courses.ENCH.ENCH201,courses.SCMT.SCMT222]],
-                [courses.ENME.ENME203,[courses.ENCI.ENCI101]],
-                [courses.SCMT.SCMT223,[courses.SCMT.SCMT211,courses.SCMT.SCMT212]],
-                [courses.ENIE.ENIE231,[courses.SCMT.SCMT211]],
-                [courses.ISLM.ISLM201,[]],
-                [courses.ENIE.ENIE203,[courses.ENIE.ENIE100]],
-            ]},
-        //semester3
-        {name:"semester3", value:[[courses.ENCH.ENCH201,[courses.SCCH.SCCH101]],
-                [courses.SCCH.SCCH213,[courses.SCCH.SCCH101]],
-                [courses.SCMT.SCMT221,[courses.SCMT.SCMT211]],
-                [courses.SCMT.SCMT222,[courses.SCMT.SCMT211]],
-                [courses.EDAR.EDAR202,[courses.EDAR.EDAR101]],
-            ]},
-        //semester2
-        {name:"semester2", value:[[courses.ENIE.ENIE255,[courses.SCMT.SCMT101]],
-                [courses.SCMT.SCMT212,[courses.SCMT.SCMT101]],
-                [courses.ENCI.ENCI101,[courses.SCPH.SCPH214]],
-                [courses.ENME.ENME102,[courses.ENME.ENME101]],
-                [courses.ENCH.ENCH110,[courses.SCCH.SCCH211]],
-                [courses.ENEE.ENEE101,[courses.CCIT.CCIT100,courses.SCMT.SCMT101]],
-                [courses.ENIE.ENIE202,[courses.ENIE.ENIE100]],
-            ]},
-        //semester1
-           {name:"semester1", value:[[courses.ENIE.ENIE100,[courses.EIPR.EIPR102]],
-                [courses.EDAR.EDAR101,[]],
-                [courses.SCMT.SCMT211,[courses.SCMT.SCMT101]],
-                [courses.SCPH.SCPH211,[courses.SCPH.SCPH101,courses.SCMT.SCMT101]],
-                [courses.SCPH.SCPH214,[courses.SCPH.SCPH101]],
-                [courses.SCCH.SCCH211,[courses.SCCH.SCCH101]],
-                [courses.ENIE.ENIE202,[]],
-            ]}
-        ];
-   */
+    ]},
+    {name:"semester4", courses:[
+        {name:["","Semester IV"]},
+        {name:allcourses.ENCH.ENCH204,pre:[allcourses.ENCH.ENCH201,allcourses.SCMT.SCMT222]},
+        {name:allcourses.ENCH.ENCH202,pre:[allcourses.ENCH.ENCH201]},
+        {name:allcourses.ENME.ENME203,pre:[allcourses.ENCI.ENCI101]},
+        {name:allcourses.SCMT.SCMT223,pre:[allcourses.SCMT.SCMT211,allcourses.SCMT.SCMT212]},
+        {name:allcourses.ENIE.ENIE231,pre:[allcourses.SCMT.SCMT211]},
+        {name:allcourses.ISLM.ISLM201,pre:[]},
+        {name:allcourses.ENIE.ENIE203,pre:[allcourses.ENIE.ENIE100]},
+    ]},
+    {name:"semester5", courses:[
+        {name:["","Semester V"]},
+        {name:allcourses.ENCH.ENCH302,pre:[allcourses.ENCH.ENCH202]},
+        {name:allcourses.ENCH.ENCH305,pre:[allcourses.ENCH.ENCH204]},
+        {name:allcourses.ENCH.ENCH306,pre:[allcourses.ENCH.ENCH204,allcourses.ENCH.ENCH305]},
+        {name:allcourses.ENCH.ENCH341,pre:[allcourses.ENCH.ENCH201,allcourses.ENCH.ENCH306]},
+        {name:allcourses.ISLM.ISLM202,pre:[allcourses.ISLM.ISLM201]},
+        {name:allcourses.SCCH.SCCH215,pre:[allcourses.SCCH.SCCH213]},
+    ]},
+    {name:"semester6",courses:[
+        {name:["","Semester VI"]},
+        {name:allcourses.ENCH.ENCH350, pre:[allcourses.ENCH.ENCH201,allcourses.SCMT.SCMT222]},
+        {name:allcourses.ENCH.ENCH330, pre:[allcourses.ENCH.ENCH302,allcourses.ENCH.ENCH306]},
+        {name:allcourses.ENCH.ENCH342, pre:[allcourses.ENCH.ENCH341]},
+        {name:allcourses.ENCH.ENCH325, pre:[allcourses.ENCH.ENCH201]},
+        {name:allcourses.ENCH.ENCH320,pre:[allcourses.ENEE.ENEE101,allcourses.SCMT.SCMT222]},
+        {name:allcourses.ENEE.ENEE305, pre:[allcourses.SCPH.SCPH211]},
+        {name:allcourses.XXXX.XXXXI,pre:[]}
+    ]},
+    
+    {name:"semester7", courses:[
+        {name:["","Summer"]},
+        {name:allcourses.ENCH.ENCH390,pre:[]},
+    ]},
+    
+    {name:"semester7", courses:[
+        {name:["","Semester VII"]},
+        {name:allcourses.ENCH.ENCH421,pre:[allcourses.ENCH.ENCH350,allcourses.ENCH.ENCH330]},
+        {name:allcourses.ENCH.ENCH420, pre:[allcourses.ENCH.ENCH330,allcourses.ENCH.ENCH325]},
+        {name:allcourses.ENCH.ENCH499,pre:[]},
+        {name:allcourses.ENCH.ENCHX,pre:[]},
+        {name:allcourses.ISLM.ISLM303, pre:[allcourses.ISLM.ISLM202]},
+        {name:allcourses.ENCH.ENCH390,pre:[]}
+    ]},
+    {name:"semester8",courses:[
+        {name:["","Semester VIII"]},
+        {name:allcourses.ENCH.ENCH422,pre:[allcourses.ENCH.ENCH330,allcourses.ENCH.ENCH420]},
+        {name:allcourses.ENCH.ENCH426,pre:[allcourses.ENCH.ENCH420]},
+        {name:allcourses.ENCH.ENCHXX,pre:[]},
+        {name:allcourses.ENCH.ENCHXXX,pre:[]},
+        {name:allcourses.XXXX.XXXXII,pre:[]}
+        ]},
+   
+]
